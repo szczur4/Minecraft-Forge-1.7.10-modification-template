f25c50d447d7bc1f45cf94f9288fafa71fa6ca83
szczur4 <125590081+szczur4@users.noreply.github.com>
2025-06-20 17:02:21 +0200

    Add files via upload

---
+++ src/main/java/neededforbuildscript/neededforbuildscript.java
@@ -0,0 +1,4 @@
+package neededforbuildscript;
+import cpw.mods.fml.common.Mod;
+@Mod(modid=neededforbuildscript.MODID,name="Needed for buildscript",acceptedMinecraftVersions="[1.7.10]")
+public class neededforbuildscript{public static final String MODID="neededforbuildscript";}
